41e5cfa2e7ba119e78edbb4d8ecf49f12eee206c
Aslı Kasap <72225773+AsliKasap@users.noreply.github.com>
2021-02-17 00:56:08 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -23,22 +23,21 @@ async function takeName(){
         var tarih = new Date();
         dateElement.innerHTML = tarih;
         if(responseJSON.weather[0].main == "Clouds"){
-            console.log(responseJSON.weather.main)
+
             weatherElement.style.backgroundImage = 'url(https://memurlar.net/common/news/images/826304/headline.jpg)';
         }if(responseJSON.weather[0].main == "Thunderstorm"){
-            console.log(responseJSON.weather.main)
+            
             weatherElement.style.backgroundImage = 'url(https://www.mercurynews.com/wp-content/uploads/2020/08/SJM-L-LIGHTNING-0817-11.jpg)';
         }if(responseJSON.weather[0].main == "Drizzle"){
-            console.log(responseJSON.weather.main)
+            
             weatherElement.style.backgroundImage = 'url(https://upload.wikimedia.org/wikipedia/commons/5/5b/Row_of_poplars_in_the_drizzle_-_geograph.org.uk_-_591822.jpg)';
         }if(responseJSON.weather[0].main == "Rain"){
-            console.log(responseJSON.weather.main)
+            
             weatherElement.style.backgroundImage = 'url(https://www.treehugger.com/thmb/6u0wbqYvi-7wxKEoueucTLDcttM=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2017__03__raindrops-plants-smell-57c4545c1bb844c0a2cd7e0252cf6d2f.jpg)';
         }if(responseJSON.weather[0].main == "Snow"){
-            console.log(responseJSON.weather.main)
+            
             weatherElement.style.backgroundImage = 'url(https://assets.climatecentral.org/images/made/2020Snow-report-cover-image_1000_664_s_c1_c_c.jpg)';
         }if(responseJSON.weather[0].main == "Clear"){
-            console.log(responseJSON.weather.main)
             weatherElement.style.backgroundImage = 'url(https://s7d2.scene7.com/is/image/TWCNews/img_3214_jpg-2)';
 
         }
